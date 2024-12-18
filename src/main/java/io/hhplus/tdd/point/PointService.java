8c13642c23e6359d4e194ea8dd9e11593de38d8f
dhlee3994 <dhlee3994@gmail.com>
2024-12-18 22:53:27 +0900

    feat: 포인트 충전/사용 내역 조회 기능 추가

--- src/main/java/io/hhplus/tdd/point/PointService.java
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -1,17 +1,28 @@
 package io.hhplus.tdd.point;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class PointService {
 
 	private final UserPointRepository userPointRepository;
+	private final PointHistoryRepository pointHistoryRepository;
 
-	public PointService(final UserPointRepository userPointRepository) {
+	public PointService(
+		final UserPointRepository userPointRepository,
+		final PointHistoryRepository pointHistoryRepository
+	) {
 		this.userPointRepository = userPointRepository;
+		this.pointHistoryRepository = pointHistoryRepository;
 	}
 
 	public UserPoint point(final long id) {
 		return userPointRepository.point(id);
+	}
+
+	public List<PointHistory> history(final long id) {
+		return pointHistoryRepository.history(id);
 	}
 }
