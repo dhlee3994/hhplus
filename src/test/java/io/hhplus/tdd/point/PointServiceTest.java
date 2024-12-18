957da3e2a7807ae9ddfdada31564ea17e6bbd176
dhlee3994 <dhlee3994@gmail.com>
2024-12-19 00:01:52 +0900

    test: 포인트 충전 기능 테스트

--- src/test/java/io/hhplus/tdd/point/PointServiceTest.java
+++ src/test/java/io/hhplus/tdd/point/PointServiceTest.java
@@ -3,8 +3,11 @@ package io.hhplus.tdd.point;
 import static io.hhplus.tdd.point.TransactionType.CHARGE;
 import static io.hhplus.tdd.point.TransactionType.USE;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.groups.Tuple.tuple;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 import java.util.List;
 
@@ -97,5 +100,77 @@ class PointServiceTest {
 
 		// then
 		assertThat(results).isEmpty();
+	}
+
+	@DisplayName("1000 포인트를 보유하고 있을 때 1000포인트를 충전하면 2000포인트가 된다.")
+	@Test
+	void charge() throws Exception {
+		// given
+		final long userId = System.currentTimeMillis();
+		final long point = 1000L;
+		final UserPoint userPoint = new UserPoint(userId, point, System.currentTimeMillis());
+		given(userPointRepository.point(userId))
+			.willReturn(userPoint);
+
+		final long amount = 1000L;
+		given(userPointRepository.charge(userId, userPoint.charge(amount)))
+			.willReturn(new UserPoint(userId, userPoint.charge(amount), System.currentTimeMillis()));
+
+		// when
+		final UserPoint result = pointService.charge(userId, amount);
+
+		// then
+		assertThat(result).isNotNull()
+			.extracting("id", "point")
+			.contains(userId, 2000L);
+		verify(pointHistoryRepository, times(1)).charge(userId, amount, result.updateMillis());
+	}
+
+	@DisplayName("양수가 아닌 포인트를 충전하려고 하면 예외가 발생한다.")
+	@Test
+	void chargeZeroPoint() throws Exception {
+		// given
+		final long userId = System.currentTimeMillis();
+		final long point = 100L;
+		final long amount = 0L;
+		given(userPointRepository.point(userId))
+			.willReturn(new UserPoint(userId, point, System.currentTimeMillis()));
+
+		// when & then
+		assertThatThrownBy(() -> pointService.charge(userId, amount))
+			.isInstanceOf(IllegalStateException.class)
+			.hasMessage("충전 포인트는 양수여야합니다.");
+	}
+
+	@DisplayName("1회 최대 충전 가능량을 초과해 충전하려고 하면 예외가 발생한다.")
+	@Test
+	void chargeOverMaxOncePoint() throws Exception {
+		// given
+		final long userId = System.currentTimeMillis();
+		final long point = 100L;
+		final long amount = 100_001L;
+		given(userPointRepository.point(userId))
+			.willReturn(new UserPoint(userId, point, System.currentTimeMillis()));
+
+		// when & then
+		assertThatThrownBy(() -> pointService.charge(userId, amount))
+			.isInstanceOf(IllegalStateException.class)
+			.hasMessage("1회 최대 충전가능량은 100,000원입니다.");
+	}
+
+	@DisplayName("최대 포인트 이상으로 충전하려고 하면 예외가 발생한다.")
+	@Test
+	void chargeOverMaxPoint() throws Exception {
+		// given
+		final long userId = System.currentTimeMillis();
+		final long point = 100_000_000L;
+		final long amount = 1L;
+		given(userPointRepository.point(userId))
+			.willReturn(new UserPoint(userId, point, System.currentTimeMillis()));
+
+		// when & then
+		assertThatThrownBy(() -> pointService.charge(userId, amount))
+			.isInstanceOf(IllegalStateException.class)
+			.hasMessageContaining("최대 포인트는 100,000,000원입니다.");
 	}
 }
