af94b2a8920102b6bf2d69ed81d694740f14b94c
dhlee3994 <dhlee3994@gmail.com>
2024-12-18 22:53:41 +0900

    test: 포인트 충전/사용 내역 조회 기능 테스트

--- src/test/java/io/hhplus/tdd/point/PointServiceIntegrationTest.java
+++ src/test/java/io/hhplus/tdd/point/PointServiceIntegrationTest.java
@@ -1,12 +1,17 @@
 package io.hhplus.tdd.point;
 
+import static io.hhplus.tdd.point.TransactionType.CHARGE;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.groups.Tuple.tuple;
+
+import java.util.List;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import io.hhplus.tdd.database.PointHistoryTable;
 import io.hhplus.tdd.database.UserPointTable;
 
 @SpringBootTest
@@ -17,6 +22,9 @@ class PointServiceIntegrationTest {
 
 	@Autowired
 	private UserPointTable userPointTable;
+
+	@Autowired
+	private PointHistoryTable pointHistoryTable;
 
 	@DisplayName("사용자의 포인트를 조회할 수 있다.")
 	@Test
@@ -47,5 +55,39 @@ class PointServiceIntegrationTest {
 		assertThat(result).isNotNull()
 			.extracting("id", "point")
 			.contains(id, 0L);
+	}
+
+	@DisplayName("사용자의 아이디로 포인트 히스토리를 조회할 수 있다.")
+	@Test
+	void history() throws Exception {
+		// given
+		final long userId = System.currentTimeMillis();
+		final long amount = 2000L;
+		final TransactionType type = CHARGE;
+		final long updateMillis = System.currentTimeMillis();
+		pointHistoryTable.insert(userId, amount, type, updateMillis);
+
+		// when
+		final List<PointHistory> results = pointService.history(userId);
+
+		// then
+		assertThat(results).hasSize(1)
+			.extracting("userId", "amount", "type", "updateMillis")
+			.containsExactlyInAnyOrder(
+				tuple(userId, 2000L, CHARGE, updateMillis)
+			);
+	}
+
+	@DisplayName("포인트 히스토리가 없는 사용자가 히스토리를 조회하면 빈 리스트를 반환한다.")
+	@Test
+	void emptyHistoryUserReturnEmptyList() throws Exception {
+		// given
+		final long userId = System.currentTimeMillis();
+
+		// when
+		final List<PointHistory> results = pointService.history(userId);
+
+		// then
+		assertThat(results).isEmpty();
 	}
 }
--- src/test/java/io/hhplus/tdd/point/PointServiceTest.java
+++ src/test/java/io/hhplus/tdd/point/PointServiceTest.java
@@ -1,7 +1,12 @@
 package io.hhplus.tdd.point;
 
+import static io.hhplus.tdd.point.TransactionType.CHARGE;
+import static io.hhplus.tdd.point.TransactionType.USE;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.groups.Tuple.tuple;
 import static org.mockito.BDDMockito.given;
+
+import java.util.List;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -18,6 +23,9 @@ class PointServiceTest {
 
 	@Mock
 	private UserPointRepository userPointRepository;
+
+	@Mock
+	private PointHistoryRepository pointHistoryRepository;
 
 	@DisplayName("사용자의 포인트를 조회할 수 있다.")
 	@Test
@@ -51,5 +59,43 @@ class PointServiceTest {
 		assertThat(result).isNotNull()
 			.extracting("id", "point")
 			.contains(id, 0L);
+	}
+
+	@DisplayName("사용자의 아이디로 포인트 히스토리를 조회할 수 있다.")
+	@Test
+	void history() throws Exception {
+		// given
+		final long userId = System.currentTimeMillis();
+		given(pointHistoryRepository.history(userId))
+			.willReturn(List.of(
+				new PointHistory(1L, userId, 2000L, CHARGE, System.currentTimeMillis()),
+				new PointHistory(2L, userId, 1000L, USE, System.currentTimeMillis())
+			));
+
+		// when
+		final List<PointHistory> results = pointService.history(userId);
+
+		// then
+		assertThat(results).hasSize(2)
+			.extracting("userId", "amount", "type")
+			.containsExactlyInAnyOrder(
+				tuple(userId, 2000L, CHARGE),
+				tuple(userId, 1000L, USE)
+			);
+	}
+
+	@DisplayName("포인트 히스토리가 없는 사용자가 히스토리를 조회하면 빈 리스트를 반환한다.")
+	@Test
+	void emptyHistoryUserReturnEmptyList() throws Exception {
+		// given
+		final long userId = System.currentTimeMillis();
+		given(pointHistoryRepository.history(userId))
+			.willReturn(List.of());
+
+		// when
+		final List<PointHistory> results = pointService.history(userId);
+
+		// then
+		assertThat(results).isEmpty();
 	}
 }
